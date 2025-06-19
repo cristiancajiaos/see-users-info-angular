f3a9a2a05528a492972c27cf2ddcdea2ee1593df
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-06-19 16:23:48 -0400

    Creado componente de página no encontrada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { CommentsComponent } from './comments/comments.component';
 import { UserDetailComponent } from './users/user-detail/user-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -16,7 +17,12 @@ export const routes: Routes = [
     path: 'comments/:id',
     component: CommentsComponent
   },
-  { path: '**',
-    redirectTo: '/users'
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found'
   }
 ];
---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [RouterModule, FontAwesomeModule],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
+})
+export class NotFoundComponent {
+  faChevronLeft: IconDefinition = faChevronLeft;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
